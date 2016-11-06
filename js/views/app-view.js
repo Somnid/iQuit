9e8a43519ad188b4cdc752ada2700791f4b5b311
Doug Kumagai <omnidestroyer@gmail.com>
2016-11-06 13:57:55 -0700

    first commit

---
+++ js/views/app-view.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var AppView = (function(){
+
+	function create(){
+		var appView = {};
+		bind(appView);
+		appView.init();
+		return appView;
+	}
+
+	function bind(appView){
+		appView.installServiceWorker = installServiceWorker.bind(appView);
+		appView.serviceWorkerInstalled = serviceWorkerInstalled.bind(appView);
+		appView.serviceWorkerInstallFailed = serviceWorkerInstallFailed.bind(appView);
+		appView.cacheDom = cacheDom.bind(appView);
+		appView.attachEvents = attachEvents.bind(appView);
+		appView.init = init.bind(appView);
+	}
+
+	function installServiceWorker(){
+		if("serviceWorker" in navigator){
+			navigator.serviceWorker.register("service-worker.js", {scope: "./"})
+				.then(this.serviceWorkerInstalled)
+				.catch(this.serviceWorkerInstallFailed);
+		}
+	}
+
+	function serviceWorkerInstalled(registration){
+		console.log("App Service registration successful with scope:", registration.scope);
+	}
+
+	function serviceWorkerInstallFailed(error){
+		console.error("App Service failed to install", error);
+	}
+
+	function cacheDom(){
+		this.dom = {};
+	}
+
+	function attachEvents(){
+	}
+
+	function getQueryData(){
+		let searchParams = new URLSearchParams(window.location.search.substr(1));
+	}
+
+	function init(){
+		this.installServiceWorker();
+		this.cacheDom();
+		this.attachEvents();
+
+		let iQuit = new SpeechSynthesisUtterance("OK Google, tell my boss 'I quit'");
+		window.speechSynthesis.speak(iQuit);
+	}
+
+	return {
+		create : create
+	};
+
+})();
